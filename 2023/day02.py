f3a94dc062683c397aa0a953871f502d8defd5b0
Emmanuel Bizieau <emmanuel.bizieau@orange.com>
2023-12-03 00:24:23 +0100

    day 02

---
+++ 2023/day02.py
@@ -0,0 +1,44 @@
+import re
+import sys
+from utils import readinput
+
+data = readinput("day02")
+
+AVAILABLE_RED, AVAILABLE_GREEN, AVAILABLE_BLUE = 12, 13, 14
+
+GAME_PATTERN = re.compile(r"Game (\d+): (.*)")
+RED_PATTERN = re.compile(r"(\d+) red")
+GREEN_PATTERN = re.compile(r"(\d+) green")
+BLUE_PATTERN = re.compile(r"(\d+) blue")
+
+possible_games = []
+powers = []
+
+for line in data:
+    match = GAME_PATTERN.match(line)
+    if not match:
+        print(f"Line not matching: {line}")
+        sys.exit(1)
+    
+    game_id = int(match.group(1))
+    draws = match.group(2).split('; ')
+
+    max_r, max_g, max_b = 0, 0, 0
+
+    for d in draws:
+        if match := RED_PATTERN.search(d):
+            max_r = max(max_r, int(match.group(1)))
+        if match := GREEN_PATTERN.search(d):
+            max_g = max(max_g, int(match.group(1)))
+        if match := BLUE_PATTERN.search(d):
+            max_b = max(max_b, int(match.group(1)))
+
+    if max_r <= AVAILABLE_RED and max_g <= AVAILABLE_GREEN and max_b <= AVAILABLE_BLUE:
+        possible_games.append(game_id)
+
+    powers.append(max_r * max_g * max_b)
+    
+# print(possible_games)
+print(f"Part one: {sum(possible_games)}")
+# print(powers)
+print(f"Part two: {sum(powers)}")
--- 2023/utils.py
+++ 2023/utils.py
@@ -6,11 +6,14 @@ AOC2023 = Path(os.path.dirname(os.path.abspath(__file__)))
 INPUTS_DIR = AOC2023 / "inputs"
 
 def readinput(day):
+    """Returns a list, with just each line as an item"""
     with (INPUTS_DIR / day).open("r") as f:
         return [line.strip() for line in f.readlines()]
 
 
 def splittedinput(day, sep=None):
+    """Each item is a line but as a list of the elements splitted around sep
+    (if sep=None, splitted around clusters of whitespaces)"""
     return [line.split(sep) for line in readinput(day)]
 
 

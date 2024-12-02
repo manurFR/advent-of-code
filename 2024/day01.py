de03f6955c8616924aa950add4cbc59e978a566a
Emmanuel Bizieau <emmanuel.bizieau@orange.com>
2024-12-02 11:56:33 +0100

    2024 day 01 part 2

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,18 +1,10 @@
+from collections import Counter
 import os
 import pytest
+
 from utils import splittedinput
 
-# Part 1
-def part1(data):
-    left, right = zip(*data)
-    left = sorted(int(n) for n in left)
-    right = sorted(int(n) for n in right)
-    return sum(abs(litem - ritem) for litem, ritem in zip(left, right))
-
-
-# Tests
-def test_part1():
-    data = [line.split() for line in """
+TESTDATA = [line.split() for line in """
 3   4
 4   3
 2   5
@@ -20,7 +12,34 @@ def test_part1():
 3   9
 3   3
 """.strip().split("\n")]
-    assert part1(data) == 11
+
+
+def parselists(data):
+    left, right = zip(*data)
+    left = sorted(int(n) for n in left)
+    right = sorted(int(n) for n in right)
+    return left, right
+
+
+# Part 1
+def part1(data):
+    left, right = parselists(data)
+    return sum(abs(litem - ritem) for litem, ritem in zip(left, right))
+
+
+def part2(data):
+    left, right = parselists(data)
+    counter_right = Counter(right)
+    return sum(n * counter_right[n] for n in left)
+
+
+# Tests
+def test_part1():
+    assert part1(TESTDATA) == 11
+
+
+def test_part2():
+    assert part2(TESTDATA) == 31
 
 
 if __name__ == "__main__":
@@ -28,3 +47,4 @@ if __name__ == "__main__":
 
     data = splittedinput(2024, "01")
     print(f"Part 1: {part1(data)}")
+    print(f"Part 2: {part2(data)}")

7798ac356fd3c166edf25fa187213b943b7001cf
manur <>
2025-12-09 11:29:59 +0100

    day08 part 2 and a bit of optim

--- 2025/day08.py
+++ 2025/day08.py
@@ -29,40 +29,35 @@ def dist3d(a, b):
     return (b[2] - a[2])**2 + (b[1] - a[1])**2 + (b[0] - a[0])**2
 
 
-def closest_pair(distances):
-    min_dist = float('inf')
-    min_pair = (-1, -1)
-    for i in range(len(distances)):
-        for j in range(i + 1, len(distances)):
-            if distances[i][j] < min_dist:
-                min_dist = distances[i][j]
-                min_pair = (i, j)
-    return min_pair, min_dist
+def prepare_distances(data):
+    """ Build the half matrix of distances, but store them as a dict 
+        with each distance as key and the list of point pairs at that distance as value
+    """
+    distances = {}  # {distance: [(i1, j1), (i2, j2), ...]}
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            d = dist3d(data[i], data[j])
+            if d not in distances:
+                distances[d] = []
+            distances[d].append((i, j))
+    return distances
 
 
-@timeit
-def part1(data, stop_at: int):
-    # build the half matrix of distances
-    n = len(data)
-    distances = [[0]*n for _ in range(n)]
-    for i in range(n):
-        for j in range(i+1, n):
-            distances[i][j] = dist3d(data[i], data[j])
-
-    # print(' '.join(f"{i:>7}" for i in range(n)))
-    # print('\n'.join(' '.join(f"{distances[i][j]:>7}" if j > i else f"{distances[j][i]:>7}" for j in range(n)) for i in range(n)))
-    
-    circuits = [[i] for i in range(n)]  # each circuit starts with one point
+def connect_boxes(data: list[list[list[int]]], distances: dict[int: list[tuple[list[int], list[int]]]], end_condition: callable) -> list[list[int]]:
+    """ Connect boxes based on distances until end_condition is met.
+        Returns the list of circuits (each circuit is a list of point indices).
+    """
+    circuits = [[i] for i in range(len(data))]  # each circuit starts with one point
     nb_connections = 0
     
-    ignore_pairs = set()
-    while nb_connections < stop_at:
-        (p1, p2), _ = closest_pair(distances)
+    p1, p2 = -1, -1
+    while len(distances) > 0 and not end_condition(circuits, nb_connections):
+        nb_connections += 1
+        min_dist = min(distances.keys())
+        p1, p2 = distances[min_dist][0]
         if any(True for c in circuits if p1 in c and p2 in c):
             # both points already in the same circuit
-            nb_connections += 1
-            # print(f"Points {data[p1]} and {data[p2]} are already connected. Skipping. Total connections: {nb_connections}.")
-            distances[p1][p2] = float('inf')  # ignore this pair next time
+            del distances[min_dist]
             continue
         # find the circuits containing p1 and p2
         idx_p1 = next(idx for idx, c in enumerate(circuits) if p1 in c)
@@ -70,11 +65,21 @@ def part1(data, stop_at: int):
         # merge circuits
         circuits[idx_p1].extend(circuits[idx_p2])
         del circuits[idx_p2]
-        nb_connections += 1
-        distances[p1][p2] = float('inf')  # ignore this pair next time
-        # print(f"Connected points {data[p1]} and {data[p2]} with distance {min_dist}. Total connections: {nb_connections}. "
-        #       f"Circuit sizes: {sorted([len(c) for c in circuits], reverse=True)}")
-        
+        del distances[min_dist]
+    
+    return circuits, (p1, p2)
+
+
+@timeit
+def part1(data, stop_at: int):
+    distances = prepare_distances(data)
+    
+    circuits, _ = connect_boxes(
+        data, 
+        distances, 
+        end_condition=lambda circuits, nb_connections: nb_connections >= stop_at
+    )
+    
     # extract the size of the circuits, sort it in descending order
     circuit_sizes = sorted([len(c) for c in circuits], reverse=True)
     # print(f"Circuit sizes: {circuit_sizes}")
@@ -89,7 +94,14 @@ def part1(data, stop_at: int):
 
 @timeit
 def part2(data):
-    return 0
+    distances = prepare_distances(data)
+    circuits, (last_p1, last_p2) = connect_boxes(
+        data, 
+        distances, 
+        end_condition=lambda circuits, nb_connections: len(circuits) == 1  # stop when all points are connected
+    )
+    
+    return data[last_p1][0] * data[last_p2][0]
 
 
 # Tests
@@ -98,22 +110,12 @@ def test_part1():
 
 
 def test_part2():
-    assert part2(TESTDATA) == 0
+    assert part2(TESTDATA) == 25272
     
     
 def test_dist3d():
     assert dist3d((0,0,0), (1,1,1)) == 3
     assert dist3d((1,2,3), (4,5,6)) == 27
-    
-
-def test_closest_pair():
-    distances = [
-        [0, 6, 4, 9],
-        [0, 0, 2, 3],
-        [0, 0, 0, 5],
-        [0, 0, 0, 0]
-    ]
-    assert closest_pair(distances) == ((1, 2), 2)
 
 
 if __name__ == "__main__":
